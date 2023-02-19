934e8bc04ef742510180f23037f55070adcb6a65
Adetola-Oluwaseyi <seyiadetola@outlook.com>
2023-02-19 14:44:42 -0800

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,9 @@
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char stor[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fprintf(stderr, "%s\n", stor);
 	return (1);
 }
 
