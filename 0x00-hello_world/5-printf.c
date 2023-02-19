cea7280f15ea4654b6b7677b15b12a8180666a10
Adetola-Oluwaseyi <seyiadetola@outlook.com>
2023-02-19 13:52:08 -0800

    Update 5-printf.c & 6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,16 @@
 #include<stdio.h>
+
+/**
+ * main - main function
+ *
+ * Description: prints a string to the standard outpupt
+ *
+ * Return: Always 0 (success)
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
 
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,13 @@
 #include<stdio.h>
+
+/**
+ * main - main function
+ *
+ * Description: Prints the sizes of various datatypes to the standard output
+ *
+ * Return: Always 0 (success)
+ */
+
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
@@ -7,5 +16,6 @@ int main(void)
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
-	return(0);
+	return (0);
 }
+
