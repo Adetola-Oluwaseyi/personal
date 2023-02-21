2c166970cce8999550f3f782294821b8bfe436af
Adetola-Oluwaseyi <seyiadetola@outlook.com>
2023-02-21 11:12:18 -0800

    first commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+void print_alphabet(void)
+
+/**
+ * main - entry point
+ *
+ * Description: calls the print_alphabet function
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	(for a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+}
