e24a489f7203f72e41e43719a95e7b0a6978f85b
Adetola-Oluwaseyi <seyiadetola@outlook.com>
2023-02-22 13:09:07 -0800

    Add 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * fibo_num - prints the first 50 Fibonacci numbers
+ */
+void fibo_num(void)
+{
+	int i;
+	unsigned long next;
+	unsigned long int first = 1;
+	unsigned long int second = 2;
+
+	printf("%ld, %ld, ", first, second);
+	for (i = 0; i < 50; i++)
+	{
+		next = first + second;
+		if (i == 49)
+		{
+			printf("%ld", next);
+		}
+
+		else
+		{
+			printf("%ld, ", next);
+		}
+		first = second;
+		second = next;
+	}
+	printf("\n");
+}
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	fibo_num();
+	return (0);
+}
