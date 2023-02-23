ce76519196503d1478cca8be3b15e052486ed828
Adetola-Oluwaseyi <seyiadetola@outlook.com>
2023-02-23 09:48:26 -0800

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,13 +5,19 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, k;
 
 	for (i = 0; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(i + '0');
+			if (j >= 10)
+			{
+				_putchar(1 + '0');
+			}
+			int k = j % 10;
+
+			_puthcar(k + '0');
 		}
 		_putchar('\n');
 	}
