a14cdb6ce580b56171a0e8ce61b0dd44f2475c3a
Adetola-Oluwaseyi <seyiadetola@outlook.com>
2023-02-28 12:18:16 -0800

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,17 +8,18 @@ void print_rev(char *s)
 {
 	int i, count;
 
-	count = 0;
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		count = i;
 	}
 
-	for (; count >= 0; count--)
+	if (count >= 0)
 	{
-		_putchar(s[count]);
+		for (; count >= 0; count--)
+		{
+			_putchar(s[count]);
+		}
 	}
-
 	_putchar('\n');
 }
 
