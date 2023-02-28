e188b11e9205918192d707ebf8b49b9d881f3552
Adetola-Oluwaseyi <seyiadetola@outlook.com>
2023-02-28 13:27:14 -0800

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,18 +8,19 @@ void rev_string(char *s)
 {
 	int i, count;
 	int j = 0;
+	char b[];
 
 	count = 0;
-	if (s != '\0')
+	if (s[0] != '\0')
 	{
-		for (i = 0, s[i] != '\0'; i++)
+		for (i = 0; s[i] != '\0'; i++)
 		{
 			count = i;
 		}
 
 		for (; count >= 0; count--)
 		{
-			char b[j] = s[count];
+			b[j] = s[count];
 
 			j++;
 		}
