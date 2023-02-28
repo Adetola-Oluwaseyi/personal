bfcb21e61572e4f6607677e0d96c333f7625233a
Adetola-Oluwaseyi <seyiadetola@outlook.com>
2023-02-28 08:36:06 -0800

    Add 4-pint_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string
+ */
+void print_rev(char *s)
+{
+	int i, count;
+
+	count = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count = i;
+	}
+
+	for (; count >= 0; count--)
+	{
+		_putchar(s[count]);
+	}
+}
+
