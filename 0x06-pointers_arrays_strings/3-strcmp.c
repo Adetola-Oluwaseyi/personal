2d9f3cb327dca9a7c16ac221939f8586c74ad6b3
Adetola-Oluwaseyi <seyiadetola@outlook.com>
2023-03-03 09:37:07 -0800

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,13 +14,13 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-        for (i = 0; s1[i] != '\0'; i++)
-        {
-                if (s1[i] < s2[i] || s1[i] > s2[i])
-                {
-                    return ((unsigned char)s1[i] - (unsigned char) s2[i]);
-                }
-        }
-        return (0);
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		if (s1[i] < s2[i] || s1[i] > s2[i])
+		{
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		}
+	}
+	return (0);
 }
 
