139dd382989f2e8812300126c79e1b176dbd6f64
Adetola-Oluwaseyi <seyiadetola@outlook.com>
2023-03-03 03:56:04 -0800

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,9 +12,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
-		if (src[i] == '\0' || dest[i] == '\0')
+		if (src[i] == '\0' && dest[i] == '\0')
 		{
 			return (dest);
 		}
