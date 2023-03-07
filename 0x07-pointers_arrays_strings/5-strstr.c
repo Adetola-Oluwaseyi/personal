ad9f29f6b7bea991ff571b437ca224a47e90faa0
Adetola-Oluwaseyi <seyiadetola@outlook.com>
2023-03-07 14:49:31 -0800

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,8 +2,8 @@
 
 /**
  * _strstr - locates a substring
- * @s: string to be scanned
- * @accept: string containing characters to match
+ * @haystack: string to be scanned
+ * @needle: string containing characters to match
  *
  * Return: pointer to the beginning of the located substring
  * NULL if substring is not found
@@ -11,29 +11,31 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int i, j, count;
-  
-  for (i = 0; needle[i] != '\0'; i++)
-  {
-    count = i;
-  }
+
+	for (i = 0; needle[i] != '\0'; i++)
+	{
+		count = i;
+	}
+
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-    if(haystack[i] == needle[0])
-    {
-         j = 0;
-         while(needle[j] != '\0' && haystack[i + j] != '\0')
-         {
-           if (haystack[i + j] != needle[j])
-           {
-             break;
-           }
-           if (j == count)
-           {
-             return (&haystack[i]);
-           }
-           j++;
-         }
-    }
-  }
-  return ('\0');
+		if (haystack[i] == needle[0])
+		{
+			j = 0;
+			while (needle[j] != '\0' && haystack[i + j] != '\0')
+			{
+				if (haystack[i + j] != needle[j])
+				{
+					break;
+				}
+				if (j == count)
+				{
+					return (&haystack[i]);
+				}
+				j++;
+			}
+		}
+	}
+	return ('\0');
 }
+
