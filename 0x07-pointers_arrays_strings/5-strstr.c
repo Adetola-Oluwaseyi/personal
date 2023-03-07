516120caf3e575430f98012a20a0e12cffd80511
Adetola Oluwaseyi <62298760+Adetola-Oluwaseyi@users.noreply.github.com>
2023-03-07 23:39:21 +0100

    Add 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @s: string to be scanned
+ * @accept: string containing characters to match
+ *
+ * Return: pointer to the beginning of the located substring
+ * NULL if substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, count;
+  
+  for (i = 0; s[i] != '\0'; i++)
+  {
+    count = i;
+  }
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+    if(haystack[i] == needle[0])
+    {
+         j = 0;
+         while(needle[j] != '\0' && haystack[i + j] != '\0')
+         {
+           if (haystack[i + j] != needle[j])
+           {
+             break;
+           }
+           if (j == count)
+           {
+             return (&haystack[i]);
+           }
+           j++;
+         }
+    }
+  }
+  return ('\0');
+}
