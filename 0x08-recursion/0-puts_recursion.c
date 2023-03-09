ff5b8e029bc48bef82799363f3f7b32a71460e13
Adetola-Oluwaseyi <seyiadetola@outlook.com>
2023-03-09 05:31:30 -0800

    Add 0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: string that is printed
+ */
+void _puts_recursion(char *s)
+{
+	if (!*s)
+	{
+		return;
+	}
+	_putchar(s[0]);
+	_puts_recursion(s + 1);
+
+}
+
