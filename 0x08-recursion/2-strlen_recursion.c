ac58b43f63a79bed11cc94f86ed6dd9356aaa667
Adetola-Oluwaseyi <seyiadetola@outlook.com>
2023-03-09 06:37:10 -0800

    Update 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - determines the length of a string
+ * @s: string that is counted
+ *
+ * Return: number of bytes in the string pointed to by s
+ */
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+	return( 1 + _strlen_recursion(s + 1));
+}
+
