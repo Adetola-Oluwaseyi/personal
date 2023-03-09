c78c5c64e39fdc7d2599a53f609fd00f6136d46d
Adetola-Oluwaseyi <seyiadetola@outlook.com>
2023-03-09 09:11:47 -0800

    Add 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,51 @@
+#include "main.h"
+int find_root(int, int);
+
+/**
+ * _sqrt_recursion - finds the natural square root of a given number
+ * @n: integer whose square root is found
+ *
+ * Return: sqaure root of a given number
+ * -1 if n has no natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	int root = 0;
+
+	root = find_root(n, root);
+	return (root);
+}
+
+/**
+ * find_root - function performing the actual computation
+ * @n: integer whose sqaure root is found
+ * @root: intial 0 zero that is potentially used to store root
+ *
+ * Return: square of given number
+ * -1 if n has no natural square root
+ */
+int find_root(int n, int root)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (0);
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
+	if (root > n / 2)
+	{
+		return (-1);
+	}
+	if (root * root == n)
+	{
+		return (root);
+	}
+	find_root(n, root + 1);
+}
+
