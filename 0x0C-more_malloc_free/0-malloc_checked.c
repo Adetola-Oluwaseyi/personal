040718eb24dfd4302db56ec381407f839d6469dc
Adetola-Oluwaseyi <seyiadetola@outlook.com>
2023-03-16 10:11:52 -0700

    Add files

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked -  allocates memory using malloc
+ * @b: amount of memory to be allocated
+ *
+ * Return: pointer to allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+
+	if (b == 0)
+		return (NULL);
+
+	p = malloc(b);
+	if (p == NULL)
+		return (NULL);
+
+	free(p);
+	return (malloc(b));
+}
+
