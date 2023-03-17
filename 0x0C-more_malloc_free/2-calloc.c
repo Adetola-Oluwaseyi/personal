c417ec0fe01aea71eb7dd66ac4ee3a830fef3a96
Adetola-Oluwaseyi <seyiadetola@outlook.com>
2023-03-17 03:05:35 -0700

    Add 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array, using malloc
+ * and sets the memory to 2
+ * @nmemb: no of elements in array
+ * @size: size in bytes of each element
+ *
+ * Return: pointer to allocated memory
+ * NULL if malloc fails and nmemb or size is 0
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *p, i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	size--;
+	for (i = 0; i < (int)size; i++)
+		p[size] = 0;
+
+	return (p);
+}
+
