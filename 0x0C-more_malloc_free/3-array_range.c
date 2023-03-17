dbc3b8e4f167db7d300b98aa7b4bdfacd6d51313
Adetola Oluwaseyi <62298760+Adetola-Oluwaseyi@users.noreply.github.com>
2023-03-17 17:11:50 +0100

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,8 +16,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	for (i = min; i <= max; i++)
-		count++;
+	count = max - min + 1;
 	p = malloc(count * sizeof(int));
 	if (p == NULL)
 		return (NULL);
@@ -29,4 +28,3 @@ int *array_range(int min, int max)
 
 	return (p);
 }
-
