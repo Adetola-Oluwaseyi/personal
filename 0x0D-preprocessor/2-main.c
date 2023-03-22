a4a2633a151ab66495f90f3c63e2728c14925997
Adetola-Oluwaseyi <seyiadetola@outlook.com>
2023-03-21 17:08:50 -0700

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -5,11 +5,11 @@
  * main - prints the name of the file it is
  * compiled from, followed by a new line
  *
- * Exit: always 0 (successful)
+ * Return: always 0 (successful)
  */
 int main(void)
 {
 	printf("%s\n", __FILE__);
-	exit(EXIT_SUCCESS);
+	return (0);
 }
 
