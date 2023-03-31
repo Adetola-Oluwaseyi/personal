9d6601da90e487e07e3c58bde4ecf23d0df21b03
Adetola-Oluwaseyi <seyiadetola@outlook.com>
2023-03-31 11:16:21 -0700

    Update 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to head node of the list
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp, *had;
+
+	tmp = head;
+	while (tmp->next)
+	{
+		had = tmp;
+		tmp = tmp->next;
+		free(had->str);
+		free(had);
+	}
+	free(tmp->str);
+	free(tmp);
+}
+
