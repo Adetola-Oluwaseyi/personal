45279ec799d2c4cb0564d91b154b9324038024dc
Adetola Oluwaseyi <62298760+Adetola-Oluwaseyi@users.noreply.github.com>
2023-03-31 16:52:07 +0100

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,28 +9,35 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new, *tmp;
-	unsigned int i;
-	int l;
+        list_t *ten;
+        list_t *tmp;
+        int i;
 
-	tmp = *head;
-	while (tmp->next != NULL)
-		tmp = (*tmp).next;
-	if (str == NULL)
-		return (NULL);
+        if (str == NULL)
+                return (NULL);
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
+        if (*head != NULL)
+        {
+            tmp = *head;
+            while (tmp != NULL)
+                tmp = (*tmp).next;
+        }
+        
+        ten = malloc(sizeof(list_t));
+        if (ten == NULL)
+                return (NULL);
 
-	new->str = strdup(str);
-	for (i = 0; str[i]; i++)
-	{
-	}
-	new->len = i;
-	new->next = NULL;
-	tmp->next = new;
+        ten->str = strdup(str);
+        for (i = 0; str[i]; i++)
+        {
+        }
+        ten->len = i;
+        ten->next = NULL;
 
-	return (new);
+        if (*head == NULL)
+        {
+            *head = ten;
+        }
+        return (ten);
 }
 
