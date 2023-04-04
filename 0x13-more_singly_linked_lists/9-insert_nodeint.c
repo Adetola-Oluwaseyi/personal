7687d31728eeefebf6561d756abd89236ed39a19
Adetola Oluwaseyi <62298760+Adetola-Oluwaseyi@users.noreply.github.com>
2023-04-04 19:15:57 +0100

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,15 +22,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	if (idx > 0)
-		idx--;
-	else
+	if (idx == 0)
 	{
 		new->next = *head;
 		*head = new;
 		return (new);
 	}
-	for (i = 0; i < idx && tmp->next; i++)
+	for (i = 0; i < (idx - 1) && tmp->next; i++)
 	{
 		tmp = tmp->next;
 	}
