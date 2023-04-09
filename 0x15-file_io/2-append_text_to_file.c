b73152b98b9494813d9691200adbf44e1c5ac62c
Adetola-Oluwaseyi <seyiadetola@outlook.com>
2023-04-09 10:02:33 -0700

    update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,6 +1,11 @@
 #include "main.h"
 
 /**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: name of file appended to
+ * @text_content: NULL terminated string to add at the end of the file
+ *
+ * Return: 1 on success and -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
