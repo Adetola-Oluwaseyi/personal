9f687c127f1f9f2777944e917ec56dfa024c5f47
Adetola Oluwaseyi <62298760+Adetola-Oluwaseyi@users.noreply.github.com>
2023-04-10 22:14:24 +0100

    update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,14 +1,12 @@
 #include "main.h"
 
-/**
- */
 unsigned int weight(unsigned int a)
 {
 	unsigned int i, ans;
-
+	
 	ans = 1;
 	for (i = 0; i < a; i++)
-		ans *= 2;
+	    ans *= 2;
 	return (ans);
 }
 /**
@@ -17,7 +15,7 @@ unsigned int binary_to_uint(const char *b)
 {
 	char *bina = (char *)b;
 	unsigned int i, num, in, ans;
-
+	
 	ans = 0;
 	if (bina == NULL)
 		return (0);
@@ -25,13 +23,13 @@ unsigned int binary_to_uint(const char *b)
 	{
 	}
 	num = i;
-	for (i = 0; num; i++)
+	for (i = 0; bina[i]; i++)
 	{
-		if (bina[num] != '0' || bina[num] != '1')
-			return (0);
-		in = bina[num] - 48;
-		ans += in * weight(num);
 		num--;
+		if (!(bina[i] == '0') && !(bina[i] == '1'))
+			return (0);
+		in = bina[i] - 48;
+		ans += in * weight(num);
 	}
 	return (ans);
 }
