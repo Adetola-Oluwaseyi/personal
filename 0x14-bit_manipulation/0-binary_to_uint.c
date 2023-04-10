b66a757bdd4840d8ff924e1b67e0f7a54e232102
Adetola-Oluwaseyi <seyiadetola@outlook.com>
2023-04-10 14:20:55 -0700

    update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,21 +1,32 @@
 #include "main.h"
 
+/**
+ * weight - helps calculate value of operand
+ * used in converting from binary to decimal
+ * @a: int argument
+ *
+ * Return: operand value
+ */
 unsigned int weight(unsigned int a)
 {
 	unsigned int i, ans;
-	
+
 	ans = 1;
 	for (i = 0; i < a; i++)
-	    ans *= 2;
+		ans *= 2;
 	return (ans);
 }
 /**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: pointer to binary value
+ *
+ * Return: converted integer
  */
 unsigned int binary_to_uint(const char *b)
 {
 	char *bina = (char *)b;
 	unsigned int i, num, in, ans;
-	
+
 	ans = 0;
 	if (bina == NULL)
 		return (0);
